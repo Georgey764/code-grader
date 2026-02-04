2b867b14cb3200da27c10a0b96c07e757d1739ba
Sujan Shrestha <shresthas4@warhawks.ulm.edu>
2026-02-04 11:42:12 -0600

    test

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    firstName = models.CharField(max_length=50)
+    lastName = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    age = models.PositiveBigIntegerField()
+    createdAt = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.firstName} {self.lastName}"
