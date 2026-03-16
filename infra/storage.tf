87893774f3275a46c4fc614b84a9dda53e322e63
Georgey764 <georgesamuel764@gmail.com>
2026-03-16 13:14:02 -0500

    Added baceknd stuff

--- infra/storage.tf
+++ infra/storage.tf
@@ -16,6 +16,33 @@ resource "aws_s3_bucket_ownership_controls" "grader_storage_controls" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.grader_storage.id
+
+  block_public_acls       = true 
+  ignore_public_acls      = true
+  block_public_policy     = false # Allows us to attach the public policy
+  restrict_public_buckets = false 
+}
+
+resource "aws_s3_bucket_policy" "allow_public_read" {
+  bucket = aws_s3_bucket.grader_storage.id
+  depends_on = [aws_s3_bucket_public_access_block.public_access]
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadEverything"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.grader_storage.arn}/*"
+      }
+    ]
+  })
+}
+
 # 1. The IAM Role
 resource "aws_iam_role" "grader_engine_role" {
   name = "grader-engine-s3-role"
